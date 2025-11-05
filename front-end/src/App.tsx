3ed4aa40fb35d1da01e1a193bc988d67a0892444
zerivan <zeri41@hotmail.com>
2025-11-05 09:21:42 -0300

    Update App.tsx

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,1 +1,12 @@
+import React from 'react'
 
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚀 Front-end funcionando com sucesso!</h1>
+      <p>Conectado ao backend na Render!</p>
+    </div>
+  )
+}
+
+export default App
