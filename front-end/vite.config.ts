ae0b416f6adf67bd4763a1e6e2099c634d2e8f97
zerivan <zeri41@hotmail.com>
2025-11-05 10:00:18 -0300

    Update vite.config.ts

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -1,13 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ✅ Configuração atualizada — usa a API ESM moderna
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-  },
-  server: {
-    port: 5173,
   },
 })
